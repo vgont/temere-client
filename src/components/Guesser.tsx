5c569f8a7c974b698cb992efcddeedc4a5b202df
Alta-ViniciusSantos <vinicius.gontijo@altasolucoes.com.br>
2025-03-10 22:54:06 -0300

    feat (Guesser): hints

--- src/components/Guesser.tsx
+++ src/components/Guesser.tsx
@@ -1,15 +1,16 @@
 import { useEffect, useState } from "react"
+
 import Form from "./Form"
 
 export default function Guesser({ room }: { room: WebSocket }) {
   const [guess, setGuess] = useState<string>("")
   const [isWordSet, setIsWordSet] = useState(false)
   const [hints, setHints] = useState<string[]>([])
-  
+
   useEffect(() => {
     const handleMessage = (event: MessageEvent) => {
       const parsed = JSON.parse(event.data)
-      
+
       if (parsed.message) {
         console.log(parsed.message)
         if (parsed.message === "word set") {
@@ -29,7 +30,7 @@ export default function Guesser({ room }: { room: WebSocket }) {
     e.preventDefault()
     room.send(`try_guess:${guess}`)
   }
-  
+
   return (
     <>
       <h1 className="text-2xl pb-4">Guesser</h1>
@@ -45,7 +46,21 @@ export default function Guesser({ room }: { room: WebSocket }) {
             inputValue={guess}
             inputHandler={setGuess}
           />
-          {hints.length > 0 && <p className="text-lg">Hints: {hints.join(", ")}</p>}
+          {hints.length > 0 &&
+            <div>
+              <h2 className="text-lg font-bold my-2">Hints</h2>
+              <div className="flex gap-4 w-full h-full flex-wrap">
+                {hints.map(hint =>
+                  <p
+                    className="text-lg text-black bg-gray-300 rounded px-2"
+                    key={hint}
+                  >
+                    {hint}
+                  </p>
+                )}
+              </div>
+            </div>
+          }
         </>
       }
     </>
